aa34f4908403affbea178293ce53109ee7f23d2f
Juan Castano <juan.castano@techin.site>
2020-10-19 15:07:56 +1100

    Adding more data

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,29 +1,32 @@
-const path = require('path')
-const resolveFrom = require('resolve-from')
+const path = require("path");
+const resolveFrom = require("resolve-from");
 
-const fixLinkedDependencies = config => {
-  config.resolve = {
-    ...config.resolve,
-    alias: {
-      ...config.resolve.alias,
-      react$: resolveFrom(path.resolve('node_modules'), 'react'),
-      'react-dom$': resolveFrom(path.resolve('node_modules'), 'react-dom'),
-    },
-  }
-  return config
-}
+const fixLinkedDependencies = (config) => {
+	config.resolve = {
+		...config.resolve,
+		alias: {
+			...config.resolve.alias,
+			react$: resolveFrom(path.resolve("node_modules"), "react"),
+			"react-dom$": resolveFrom(
+				path.resolve("node_modules"),
+				"react-dom"
+			),
+		},
+	};
+	return config;
+};
 
-const includeSrcDirectory = config => {
-  config.resolve = {
-    ...config.resolve,
-    modules: [path.resolve('src'), ...config.resolve.modules],
-  }
-  return config
-}
+const includeSrcDirectory = (config) => {
+	config.resolve = {
+		...config.resolve,
+		modules: [path.resolve("src"), ...config.resolve.modules],
+	};
+	return config;
+};
 
 module.exports = [
-  ['use-babel-config', '.babelrc'],
-  ['use-eslint-config', '.eslintrc'],
-  fixLinkedDependencies,
-  // includeSrcDirectory,
-]
+	["use-babel-config", ".babelrc"],
+	//   ['use-eslint-config', '.eslintrc'],
+	fixLinkedDependencies,
+	// includeSrcDirectory,
+];
--- src/makeData.js
+++ src/makeData.js
@@ -1,40 +1,47 @@
-import namor from 'namor'
+import namor from "namor";
 
-const range = len => {
-  const arr = []
-  for (let i = 0; i < len; i++) {
-    arr.push(i)
-  }
-  return arr
-}
+const range = (len) => {
+	const arr = [];
+	for (let i = 0; i < len; i++) {
+		arr.push(i);
+	}
+	return arr;
+};
 
-const newPerson = () => {
-  const statusChance = Math.random()
-  return {
-    firstName: namor.generate({ words: 1, numbers: 0 }),
-    lastName: namor.generate({ words: 1, numbers: 0 }),
-    age: Math.floor(Math.random() * 30),
-    visits: Math.floor(Math.random() * 100),
-    progress: Math.floor(Math.random() * 100),
-    status:
-      statusChance > 0.66
-        ? 'relationship'
-        : statusChance > 0.33
-        ? 'complicated'
-        : 'single',
-  }
-}
+const newPerson = (additionalColumns = 0) => {
+	const statusChance = Math.random();
+	const initialData = {
+		firstName: namor.generate({ words: 1, numbers: 0 }),
+		lastName: namor.generate({ words: 1, numbers: 0 }),
+		age: Math.floor(Math.random() * 30),
+		visits: Math.floor(Math.random() * 100),
+		progress: Math.floor(Math.random() * 100),
+		status:
+			statusChance > 0.66
+				? "relationship"
+				: statusChance > 0.33
+				? "complicated"
+				: "single",
+	};
+	for (let i = 1; i <= additionalColumns; i++) {
+		initialData[`additional_column_${i}`] = namor.generate({
+			words: 1,
+			numbers: 0,
+		});
+	}
+	return initialData;
+};
 
-export default function makeData(...lens) {
-  const makeDataLevel = (depth = 0) => {
-    const len = lens[depth]
-    return range(len).map(d => {
-      return {
-        ...newPerson(),
-        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-      }
-    })
-  }
+export default function makeData(additionalColumns, ...lens) {
+	const makeDataLevel = (depth = 0) => {
+		const len = lens[depth];
+		return range(len).map((d) => {
+			return {
+				...newPerson(additionalColumns),
+				subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+			};
+		});
+	};
 
-  return makeDataLevel()
+	return makeDataLevel();
 }
